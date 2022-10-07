3ed99faa2c27abba92e597e8780ba9c4d696065f
Vel <alexei987654321@mail.ru>
2022-10-07 14:39:55 +0300

    Решил втоурю  задачу

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿ /*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
 
@@ -38,22 +38,22 @@ int[] Getarray(int size, int minValue, int maxValue)
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
 
-
-int [] array = Getarray(5, 1, 5);
+/* 
+int[] array = Getarray(5, 1, 5);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 Console.WriteLine($"Сумма элементов стоящих на нечетных позициях = {SumOdd(array)} ");
 
 
-int SumOdd (int[] array)
+int SumOdd(int[] array)
 {
-int sum = 0;
+    int sum = 0;
 
-    for (int i = 0; i < array.Length; i+=2)
+    for (int i = 0; i < array.Length; i += 2)
     {
-       sum = sum + array[i];
-    
-}
-return sum;
+        sum = sum + array[i];
+
+    }
+    return sum;
 }
 
 int[] Getarray(int size, int minValue, int maxValue)
@@ -65,3 +65,33 @@ int[] Getarray(int size, int minValue, int maxValue)
     }
     return res;
 }
+
+ */
+
+
+/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+float[] B = new float[50];
+// double[] array =  Getarray(5, 0, 100);
+Console.WriteLine($"[{string.Join(", ", B)}]");
+
+double[] Getarray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+/* void FillArrayRandomNumbers(double[] numbers, int minValue, int maxValue)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1));
+        }
+} */

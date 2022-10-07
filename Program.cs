1036a62b1d727727571dedeee7ff4e438256cc72
Vel <alexei987654321@mail.ru>
2022-10-08 01:03:12 +0300

    Решил третью  задачу

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
-
+/*
 int [] array = Getarray(5, 100, 1000);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 Console.WriteLine($"В массиве количество четных чисел = {HowManyEven(array)}");
@@ -31,14 +31,14 @@ int[] Getarray(int size, int minValue, int maxValue)
     return res;
 }
 
-
+*/
 
 
 /* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
 
-
+/*
 int[] array = Getarray(5, 1, 5);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 Console.WriteLine($"Сумма элементов стоящих на нечетных позициях = {SumOdd(array)} ");
@@ -67,7 +67,7 @@ int[] Getarray(int size, int minValue, int maxValue)
 }
 
 
-
+*/
 
 /* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
@@ -75,16 +75,16 @@ int[] Getarray(int size, int minValue, int maxValue)
 */
 
 
-double[] array = Getarray(5, 0, 100);
+int[] array = Getarray(5, 0, 100);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 // Console.WriteLine($"Diff = {Max(array) - Min(array)}");
-Console.WriteLine($"Разница между максимальным и минимальным элементом = {MAXX(array) - Min(array)}");
+Console.WriteLine($"Разница  = {MaxMin(array)}");
 
 
-
-double[] Getarray(int size, int minValue, int maxValue)
+/*
+int[] Getarray(int size, int minValue, int maxValue)
 {
-    double[] res = new double[size];
+    int[] res = new int[size];
     for (int i = 0; i < size; i++)
     {
 
@@ -93,25 +93,33 @@ double[] Getarray(int size, int minValue, int maxValue)
     }
     return res;
 }
+*/
 
 
-
-double Min(double[] array)
+int MaxMin(int[] array)
 {
-    double min = double.MaxValue;
+    int max = array[0];
+    int min = array[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] < min)
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
         {
             min = array[i];
         }
+        
+        
     }
-    return min;
+    Console.WriteLine($"MAX = {max}, MIN = {min}");
+    return max - min;
 }
 
-double MAXX(double[] array)
+int MAXX(int[] array)
 {
-    double max = double.MinValue;
+    int max = int.MinValue;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] > max)
@@ -123,6 +131,15 @@ double MAXX(double[] array)
 }
 
 
+int[] Getarray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
 
 
 
